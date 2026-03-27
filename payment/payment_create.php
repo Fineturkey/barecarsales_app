4f6a7d013b2131176a9386f6a804a90107961a2c
Ryan Luu <r.luu@uleth.ca>
2026-03-27 02:57:55 -0600

    added variables

---
+++ payment/payment_create.php
@@ -0,0 +1,17 @@
+<?php 
+include '../db.php';
+
+$errors = [];
+$sale_id = '';
+$customer_id = '';
+$payment_date ='';
+$due_date = '';
+$paid_date = '';
+$amount = '';
+$bank_account = '';
+
+$is_late = 0;
+$days_late = 0;
+
+
+?>
