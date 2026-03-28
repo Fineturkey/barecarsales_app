43c36b13a736641f9c9ef4403a1743df40b0e52b
Ryan L <thegrandtuna@gmail.com>
2026-03-27 23:50:19 -0600

    added post request for each thing

--- payment/payment_create.php
+++ payment/payment_create.php
@@ -15,5 +15,13 @@ $days_late = 0;
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sale_id = trim($_POST['sale_id'] ?? '');
+    $customer_id = trim($_POST['customer_id'] ?? '');
+    $payment_date = trim($_POST['payment_date'] ?? '');
+    $due_date = trim($_POST['due_date'] ?? '');
+    $paid_date = trim($_POST['paid_date'] ?? '');
+    $amount = trim($_POST['amount'] ?? '');
+    $bank_account = trim($_POST['bank_account'] ?? '');
+
+    
 }
 ?>
