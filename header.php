d47bae1b026f567f585ad7a6afc7b08916c3f947
Eugene <Eugene7098@gmail.com>
2026-03-29 10:13:08 -0600

    Made the main page less ugly

--- header.php
+++ header.php
@@ -1,24 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JonesAuto Admin</title>
     <link rel="stylesheet" href="/barecarsales_app/style.css">
 </head>
+
 <body>
     <div class="container">
         <h1>JonesAuto Database Interface</h1>
-        <nav>
-            <a href="/barecarsales_app/index.php">Home</a>
-            <a href="/barecarsales_app/repair/repairs.php">Manage Repairs</a>
-            <a href="/barecarsales_app/repair/repair_create.php">Add repairs</a>
-            <a href="/barecarsales_app/customer/customers.php">Manage Customers</a>
-            <a href="/barecarsales_app/customer/customer_create.php">Add Customers</a>
-            <a href="/barecarsales_app/employee/employees.php">Manage Employees</a>
-            <a href="/barecarsales_app/employee/employee_create.php">Add Employees</a>
-            <a href="/barecarsales_app/purchase/purchases.php">Manage Purchase</a>
-            <a href="/barecarsales_app/purchase/purchase_create.php">Add Purchase</a>
+        <nav class="navbar">
+            <ul class="menu">
+                <li><a href="/barecarsales_app/index.php">Home</a></li>
 
+                <li class="dropdown">
+                    <a href="#">Repairs</a>
+                    <ul class="submenu">
+                        <li><a href="/barecarsales_app/repair/repairs.php">Manage Repairs</a></li>
+                        <li><a href="/barecarsales_app/repair/repair_create.php">Add Repair</a></li>
+                        <li><a href="/barecarsales_app/repair/repair_delete.php">Delete Repair</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#">Customers</a>
+                    <ul class="submenu">
+                        <li><a href="/barecarsales_app/customer/customers.php">Manage Customers</a></li>
+                        <li><a href="/barecarsales_app/customer/customer_create.php">Add Customer</a></li>
+                        <li><a href="/barecarsales_app/customer/customer_delete.php">Delete Customer</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#">Employees</a>
+                    <ul class="submenu">
+                        <li><a href="/barecarsales_app/employee/employees.php">Manage Employees</a></li>
+                        <li><a href="/barecarsales_app/employee/employee_create.php">Add Employee</a></li>
+                        <li><a href="/barecarsales_app/employee/employee_delete.php">Delete Employee</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#">Sales</a>
+                    <ul class="submenu">
+                        <li><a href="/barecarsales_app/sale/sales.php">Manage Sales</a></li>
+                        <li><a href="/barecarsales_app/sale/sale_create.php">Add Sale</a></li>
+                        <li><a href="/barecarsales_app/sale/sale_delete.php">Delete Sale</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#">Payments</a>
+                    <ul class="submenu">
+                        <li><a href="/barecarsales_app/payment/payments.php">Manage Payments</a></li>
+                        <li><a href="/barecarsales_app/payment/payment_create.php">Add Sale</a></li>
+                        <li><a href="/barecarsales_app/payment/payment_delete.php">Add Sale</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#">Purchases</a>
+                    <ul class="submenu">
+                        <li><a href="/barecarsales_app/purchase/purchases.php">Manage Purchases</a></li>
+                        <li><a href="/barecarsales_app/purchase/purchase_create.php">Add Purchase</a></li>
+                        <li><a href="/barecarsales_app/purchase/purchase_delete.php">Delete Purchase</a></li>
+
+                        
+                    </ul>
+                </li>
+            </ul>
         </nav>
         <hr>
