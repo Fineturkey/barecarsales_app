544d91b2c0508aaf0eb2c67d5768cf12931e0c4c
Eugene <Eugene7098@gmail.com>
2026-04-07 15:00:44 -0600

    renaming

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 <h2>Welcome</h2>
 <p>What up it is Jones Auto!</p>
 
-<img src="/barecarsales_app/images/Crappycars.avif" class="hero-img" alt="Jones Auto showroom">
+<img src="/barecarsales_app/images/cars.avif" class="hero-img" alt="Jones Auto showroom">
 
 <?php include 'footer.php'; ?>
